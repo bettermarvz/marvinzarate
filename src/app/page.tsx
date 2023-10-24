a52926b4c21ce5fdd76a90bac563f3e5fae1d7a2
marvin zarate <marvs@betterteem.com>
2023-10-24 10:14:57 +0800

    feat: initial page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,81 @@
+import React from 'react'
 import Image from 'next/image'
 
-export default function Home() {
+const Main = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+    <div className='relative w-[100svw]'>
+      <div className='w-full h-[100svh] bg-[#232F28]'>
+        <div className='fixed top-0 z-0 flex items-center w-full h-full'>
+          <div className='w-1/2 font-roboto flex flex-col text-white pl-[9vw] mt-48'>
+            <p className='text-4xl'>{`Hi! I'm`} <span className='text-[#FF8700]'>Marvin Zarate</span> and</p>
+            <p className='text-9xl font-light'>{`I'm a Web`}</p>
+            <p className='text-9xl font-black'>{`Developer`}</p>
+          </div>
+          <div className='w-1/2 h-full '>
+            {/* <img src='/marvz.jpg' /> */}
+            </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='w-full h-[120svh] bg-[#FF8700]' style={{'clipPath':'polygon(0 0, 100% 0%, 100% 100%, 0% 100%)'}}>
+      {/* <div className='w-full h-[200svh] bg-[#FF8700]' style={{'clipPath':'polygon(100% 0%, 100% 100%, 0% 100%, 0% 50%)'}}> */}
+        <div className='fixed top-0 z-10 flex items-center w-full h-full'>
+          <div className='w-1/2 font-roboto flex flex-col text-white  pl-[9vw] mt-48'>
+            <p className='text-4xl'>{`Hi! I'm`} <span className='text-[#232F28]'>Marvin Zarate</span> and</p>
+            <p className='text-9xl font-extralight'>{`I'm a UI/UX`}</p>
+            <p className='text-9xl font-black'>Designer</p>
+          </div>
+          <div className='w-1/2 h-full'>
+            {/* <img src='/marvz.jpg' /> */}
+            </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className='w-full h-[120vh] sticky z-20 bg-[#E5EDF1] top-0'>
+        <div className='flex flex-col items-center gap-10 w-full h-full py-14 px-[9vw]'>
+          <p className='text-[50px]'>About</p>
+          <p className=' w-[70%] text-center'>
+            {`Marvz`} <span className='text-[#FF8700] text-xs'>{`(He/Him)`}</span>, {`I’m a versatile professional who started as a graphic designer and transitioned into Web development. I specialize in web design using Figma and Adobe XD, and I'm proficient in React.js, Next.js, and Webflow. My passion lies in creating visually stunning and highly functional websites. I'm dedicated to staying at the forefront of design and development trends to deliver innovative solutions. Let's collaborate to bring your digital vision to life!`}
+            </p>
+            <p className='text-[25px]'>Sample Works</p>
+            <div className='grid grid-flow-col gap-6 w-full'>
+              <div className='card row-span-4 bg-white h-60'>
+                sdjg
+              </div>
+              <div className='card row-span-4 bg-white h-60'>
+                sdjg
+              </div>
+              <div className='card row-span-4 bg-white h-60'>
+                sdjg
+              </div>
+            </div>
+        </div>
       </div>
-    </main>
+      <div className='w-full h-[100vh] sticky z-20 bg-[#232F28] text-white top-0'>
+        <div className='flex flex-col items-center gap-10 w-full h-full py-14 px-[9vw]'>
+          <p className='text-[50px]'>Tech stack</p>
+            <div className='grid grid-rows-2 grid-cols-4 gap-10 w-full h-full py-10'>
+                <div className='flex justify-center items-center w-full h-[250px] bg-[#00000010]'><Image alt='html logo' src='/images/html.png' height={100} width={100}/></div>
+                <div className='flex justify-center items-center w-full h-[250px] bg-[#00000010]'><Image  alt='css logo' src='/images/css.png' height={100} width={100}/></div>
+                <div className='flex justify-center items-center w-full h-[250px] bg-[#00000010]'><Image alt='javascript logo' src='/images/js.png' height={100} width={100}/></div>
+                <div className='flex justify-center items-center w-full h-[250px] bg-[#00000010]'><Image alt='react logo' src='/images/react.png' height={100} width={100}/></div>
+                <div className='flex justify-center items-center w-full h-[250px] bg-[#00000010]'><Image alt='nextjs logo' src='/images/nextjs.png' height={100} width={100}/></div>
+                <div className='flex justify-center items-center w-full h-[250px] bg-[#00000010]'><Image alt='webflow logo' src='/images/webflow.png' height={100} width={100}/></div>
+                <div className='flex justify-center items-center w-full h-[250px] bg-[#00000010]'><Image alt='wordpress logo' src='/images/wordpress.png' height={100} width={100}/></div>
+                <div className='flex justify-center items-center w-full h-[250px] bg-[#00000010]'><Image alt='mysql logo' src='/images/mysql.png' height={100} width={100}/></div>
+            </div>
+        </div>
+      </div>
+      <div className='w-full h-[100vh] sticky z-20 bg-white top-0'>
+          <div className='flex flex-col items-center gap-10 w-full h-full py-14 px-[9vw]'>
+            <p className='text-[50px]'>Contat me</p>
+            <form>
+              
+            </form>
+          </div>
+        </div>
+    </div>
+    </>
   )
 }
+
+export default Main
