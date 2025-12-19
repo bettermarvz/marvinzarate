a6103c7a789ec5306143a64db27387f9662017df
MarvZ <marvs@betterteem.com>
2025-12-19 13:34:58 +0800

    fix: time

--- app/components/ui/Time.tsx
+++ app/components/ui/Time.tsx
@@ -1,13 +1,24 @@
+'use client'
+
 import dayjs from 'dayjs'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Time = () => {
+  const [time, setTime] = useState(dayjs())
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(dayjs())
+    }, 1000) // update every second
+
+    return () => clearInterval(interval)
+  }, [])
   return (
     <div className='absolute right-0 -bottom-[100px] opacity-10'>
         <div className='flex items-end'>
             <div className='[&>p]:text-[384px] font-bold flex flex-col justify-end leading-[350px] align-bottom'>
-                <p>{dayjs().format('h')}</p>
-                <p>{dayjs().format('mm')}<span className='text-[161px]'>{dayjs().format('A')}</span></p>
+                <p>{time.format('h')}</p>
+                <p>{time.format('mm')}<span className='text-[161px]'>{time.format('A')}</span></p>
             </div>
         </div>
     </div>
